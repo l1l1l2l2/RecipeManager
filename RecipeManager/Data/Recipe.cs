09a60ecceebe9934348753010f21c552d1fb2c02
denis <okposhka09@gmail.com>
2022-07-21 22:35:18 +0700

    Base data model

--- RecipeManager/Data/Recipe.cs
+++ RecipeManager/Data/Recipe.cs
@@ -1,6 +1,17 @@
-﻿namespace RecipeManager.Data
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecipeManager.Data
 {
     public class Recipe
     {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public TimeSpan TimeToCook { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Method { get; set; }
+        public bool IsVegetarian { get; set; }
+        public string CreatedById { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set;}
     }
 }
