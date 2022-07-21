3c7fae8997f136eb14e2eeda181a7453ccfa2f2b
denis <okposhka09@gmail.com>
2022-07-21 22:48:49 +0700

    Recipe VM

---
+++ RecipeManager/Models/RecipeSummaryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeManager.Models
+{
+    public class RecipeSummaryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TimeToCook { get; set; }
+        public int NumberOfIngredients { get; set; }
+    }
+}
